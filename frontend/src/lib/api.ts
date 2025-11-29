0de640250f02bb6ff019d8a8b254a16205aeb552
kietthetran123@gmail.com <kiet.tt@vgamestd.com>
2025-11-29 17:57:44 +0700

    url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { auth } from './auth';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const API_BASE_URL ='backend-issue-production.up.railway.app';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
